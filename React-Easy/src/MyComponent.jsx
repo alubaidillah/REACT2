5d531812a61bc74d3b1dfc41a37a78694e463598
alubaidillah <alubaidillah1@gmail.com>
2025-05-09 21:56:14 +0700

    selesai Array in State

--- React-Easy/src/MyComponent.jsx
+++ React-Easy/src/MyComponent.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-  const [food, setFood] = useState("apple", "banana", "orange");
+  const [foods, setFood] = useState(["apple", "banana", "orange"]);
 
   function handleAddFood() {
     const newFood = document.getElementById("foodInput").value;
     document.getElementById("foodInput").value = "";
+
+    setFood((f) => [...f, newFood]);
   }
 
-  function handleRemoveFood() {}
+  function handleRemoveFood(index) {
+    setFood(foods.filter((_, i) => i !== index));
+  }
 
   return (
     <div>
       <h2>List of food</h2>
       <ul>
-        {food.map((food, index) => (
-          <li key={index}>{food}</li>
+        {foods.map((food, index) => (
+          <li key={index} onClick={() => handleRemoveFood(index)}>
+            {food}
+          </li>
         ))}
       </ul>
       <input type="text" id="foodInput" placeholder="Enter Food name"></input>
